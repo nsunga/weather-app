5faabf9f264c71e5789b4a4cd47f2cf557aaee97
Nicholas Sunga <nick.sunga95@gmail.com>
2018-03-30 23:50:07 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+// import 'bootstrap/dist/css/boostrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './App.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
